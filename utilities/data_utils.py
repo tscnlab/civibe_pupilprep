627d0a045461a0ec7c5fd3d9f0b1410cccfa8887
Diana Glebowicz <diana.glebowicz@gmail.com>
2024-10-29 16:46:17 +0100

    add module with data utilities

---
+++ utilities/data_utils.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def calculate_change_from_baseline(data_df):
+    data_df['Baseline change %'] = ['N/A']*len(data_df)
+    for i in data_df['Trial no'].unique()[1::]:
+        trial_df = data_df[(data_df['Trial no']==i)].copy()
+        baseline = trial_df['Stim eye - Size Mm'][trial_df['Trial time Sec']<0].mean()
+        data_df.loc[(data_df['Trial no']==i),'Baseline change %'] = (trial_df['Stim eye - Size Mm'] - baseline)*100/baseline
+    return data_df
+
+
+def plot_trials(data_df_new,participant_id, trial_types: list):  
+    fig,axs = plt.subplots(1,len(trial_types),figsize=(len(trial_types)*5,5))
+    for i,trial_type in trial_types:
+        axs[i].axvspan(0, 5, alpha=0.1, color='green')
+        sns.scatterplot(ax=axs[i],data = data_df_new[data_df_new['Trial type']==trial_type],x='Trial time Sec',y='Stim eye - Size Mm',hue='Trial no',s=0.5)
+        axs[i].set_title(trial_type)
+        if i>0:
+            axs[i].set_ylabel('')
+        axs[i].legend([],[], frameon=False)
+        axs[i].set_ylim([0,np.max(data_df_new['Stim eye - Size Mm'][data_df_new['Trial type']==trial_type])+1])
+    fig.suptitle(f'Pupil size per trial for participant {participant_id}')
+    plt.tight_layout()
+    plt.show()
+    return fig,axs
+
+
+def plot_baseline_change(data_df_new,participant_id,trial_types: list):
+    fig,axs = plt.subplots(1,len(trial_types),figsize=(len(trial_types)*5,5))
+    for i,trial_type in enumerate(sorted(data_df_new['Trial type'][data_df_new['Trial type']!='N/A'].unique())):
+        axs[i].axvspan(0, 5, alpha=0.1, color='green')
+        sns.scatterplot(ax=axs[i],data = data_df_new[data_df_new['Trial type']==trial_type],x='Trial time Sec',y='Baseline change %',hue='Trial no',s=0.5)
+        axs[i].set_title(trial_type)
+        if i>0:
+            axs[i].set_ylabel('')
+        axs[i].legend([],[], frameon=False)
+        axs[i].set_ylim([np.min(data_df_new['Baseline change %'][data_df_new['Trial type']==trial_type])-1,
+                         np.max(data_df_new['Baseline change %'][data_df_new['Trial type']==trial_type])+1])
+    fig.suptitle(f'Change in stimulated pupil size from baseline average per trial for participant {participant_id}')
+    plt.tight_layout()
+    plt.show()
+    return fig,axs
