9ab8728bbc8183bcde9a8ddc8730b1145d647e1b
Diana Glebowicz <diana.glebowicz@gmail.com>
2024-11-29 08:17:39 +0100

    change variable name

--- scripts/load_and_resample.py
+++ scripts/load_and_resample.py
@@ -22,13 +22,13 @@ def main():
     include_failed_raw = False   #do you want to include failed runs? 
     save_raw = True   #do you want to save the loaded and marked raw data and protocol files? 
     save_path_raw = './results/'   # save path for raw data, it must exist! add a slash at the end to avoid incorrect saving
-    sampling_frequency = 30   #frequency to resample to
+    resampling_frequency = 30   #frequency to resample to
     save_path_resampled = './results/resampled/'   #save path for resampled data, must exist!, filenames 2xx_{sampling freq.}_resampled_data.csv
 
     
     for participant_id in participant_list:
         
-        print(str(participant_id)+' :Loading data and resampling to '+str(sampling_frequency)+'...')
+        print(str(participant_id)+' :Loading data and resampling to '+str(resampling_frequency)+'...')
         data_df, protocol_timecourse_df, protocol_vars_df = load.load_participant_data(participant_no=participant_id,
                                                                                     data_dir=raw_data_dir,
                                                                                     include_failed=include_failed_raw,
@@ -36,10 +36,10 @@ def main():
                                                                                     save_path=save_path_raw)
         
         data_df = prep.remove_artefacts_non_physio_size(data_df)
-        data_df = prep.resample_by_trial(data_df,sample_freq = sampling_frequency)
+        data_df = prep.resample_by_trial(data_df,sample_freq = resampling_frequency)
         
         print('Saving resampled data...')
-        resampled_fp = str(participant_id)+'_'+str(sampling_frequency)+'_resampled_data.csv'
+        resampled_fp = str(participant_id)+'_'+str(resampling_frequency)+'_resampled_data.csv'
         data_df.to_csv(os.path.join(save_path_resampled,resampled_fp))
         print('Done, moving on!')
         
